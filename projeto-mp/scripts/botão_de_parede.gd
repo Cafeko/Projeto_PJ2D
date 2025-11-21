cffd29ac436e1ef9c11b66e17242a8ba3a900617
Douglas Gemir <dcg@cin.ufpe.br>
2025-11-21 14:56:52 -0300

    restore: botão_de_parede.gd restaurado

--- projeto-mp/scripts/botão_de_parede.gd
+++ projeto-mp/scripts/botão_de_parede.gd
@@ -1,27 +1,19 @@
 extends Area2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var plataforma_ativavel: StaticBody2D = $"../PlataformaAtivavel"
-
-var foi_ativado: bool = false
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	sprite.play("default")
-	print("Ponte encontrada?", plataforma_ativavel)
 
 func _on_body_entered(body):
-	if foi_ativado:
-		return  # já ativou antes, ignora
+	print("Body entrou:", body)
 
+	# Verifica se o body é o Player OU se o pai dele é o Player
 	var root = body
 	while root:
 		if root is Player:
-			foi_ativado = true   # impede novas ativações
 			print("colidiu com o player!")
 			sprite.play("pressionado")
-
-			plataforma_ativavel.toggle()
 			return
-
 		root = root.get_parent()
